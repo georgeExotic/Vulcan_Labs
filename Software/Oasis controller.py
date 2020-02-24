e0f823b6093009e5d98854f152dcb31314c20312
Jroa <roaj@tamu.edu>
2020-02-24 17:59:30 -0600

    never mind the last commit(JORGE)

--- Software/Oasis controller.py
+++ Software/Oasis controller.py
@@ -17,7 +17,7 @@
 #TESTE
 
 import sys
-sys.path.append('/usr/local/lib/python3.7/site-packages')
+# sys.path.append('/usr/local/lib/python3.7/site-packages')
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtWidgets import QFileDialog
 from PyQt5.QtWidgets import QMessageBox, QComboBox
