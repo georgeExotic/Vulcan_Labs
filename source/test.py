f6d13fa4d8bbcf7eb3cd03f622da52b27cc8b1ba
jorge <roaj@tamu.edu>
2021-03-14 13:04:13 -0500

    finish testing for negative vals

--- source/test.py
+++ source/test.py
@@ -8,18 +8,21 @@ from pyModbusTCP import utils
 def slewMotor(slew):
     print("input int = ",slew)
 
-    word = utils.long_list_to_word([slew],False)
+    complement = utils.get_2comp(slew,32)       
+    word = utils.long_list_to_word([complement])
+
+    # long = utils.word_list_to_long(complement)
+
+    # complement2 = utils.get_list_2comp(long)
+
+    # word2 = utils.long_list_to_word(complement2)
     
-    complement = utils.get_list_2comp([slew])       
+    # print(complement)
+    print("2's complement = ", complement , "word", word , "\n")
+    # print("2's complement = ", complement2, "word2 = ",word2, "\n")
 
-    print("word = ", word, "2's complement = ", complement, "\n")
-
-slewMotor(500000)
-slewMotor(41249)
-reg = 50
-print(reg)
-reg = list((reg))
-print(reg)
+slewMotor(-5000)
+slewMotor(5000)
 
 # [500,0] --> [500] word list to long
 # [500] --> [500,0] long list to word
