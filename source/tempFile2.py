d96e4790776b7450151cfcc03531bc1ff5147f16
atrejos <alextre.edu@gmail.com>
2021-04-27 20:22:54 +0100

    updated new gui

--- source/tempFile2.py
+++ source/tempFile2.py
@@ -10,10 +10,10 @@ from PyQt5.QtGui import QCursor
 from ui_main import ui_main
 
 from threadClasses import WorkerSignals, Worker
-# from LoadCell import LoadCell
-# from vulcanControl import Motor
-# import RPi.GPIO as GPIO #import I/O interface
-# from hx711 import HX711 #import HX711 class
+from LoadCell import LoadCell
+from vulcanControl import Motor
+import RPi.GPIO as GPIO #import I/O interface
+from hx711 import HX711 #import HX711 class
 
 class Ui_MainWindow(QMainWindow):
     def __init__(self):
@@ -30,7 +30,6 @@ class Ui_MainWindow(QMainWindow):
 
         self.show()
 
-
         self.motorConnected = False
         self.cellConnected = False
 
@@ -39,11 +38,27 @@ class Ui_MainWindow(QMainWindow):
         self.topLimit = False
         self.homeLimit = False
         self.positionReading = None
+        self.force_threadStarted = 0
+        self.position_threadStarted = 1
 
         self.jogging = False
 
         self.start_worker_threadManager()
-        print("running")
+
+        # UI MODIFICATIONS
+        self.m_ui.stackedWidget.setCurrentIndex(0)
+        # self.m_ui.layerBefore_comboBox.addItem('option 1')
+
+        # BUTTON FUNCTION MAPPING
+        self.m_ui.connectMotor_button.clicked.connect(self.createMotorInstance)
+        self.m_ui.connectLoad_button.clicked.connect(self.createCellInstance)
+        self.m_ui.jogUp_button.clicked.connect(self.jogUp)
+        self.m_ui.jogDown_button.clicked.connect(self.jogDown)
+        self.m_ui.home_button_page1.clicked.connect(self.home)
+        self.m_ui.stopButton_page1.clicked.connect(self.stop)
+        self.m_ui.stopButton_page2.clicked.connect(self.stop)
+        self.m_ui.runButton_page2.clicked.connect(self.startRun)
+
 
     def clear_widgets(self):
         widgets = self.widgets
@@ -53,159 +68,6 @@ class Ui_MainWindow(QMainWindow):
             for i in range(0, len(widgets[widget])):
                 widgets[widget].pop()
         self.widgets = widgets
-
-    def show_frame1(self,widgets):
-        self.clear_widgets()
-        self.frame1()
-
-    def create_buttons(self,answer):
-        button = QPushButton(answer)
-        button.setCursor(QtCore.Qt.PointingHandCursor)
-        button.setFixedWidth(100)
-        button.setStyleSheet(
-            "*{border: 4px solid '#ccc';" +
-            "border-radius: 15px;" +
-            "font-size: 14px;" +
-            "color: 'white';" +
-            "padding: 5px 5px;" +
-            "margin: 0px 0px;}" + 
-            "*:hover{background: '#369';}")
-        return button
-
-    def create_label(self,label_name):
-        label = QLabel(label_name)
-        label.setStyleSheet(
-            "color: #fff;"
-            "font: 14px;")
-        return label
-
-    def frame1(self):
-        #title widget
-        title = QLabel("Test Program")
-        title.setAlignment(QtCore.Qt.AlignCenter)
-        title.setStyleSheet(
-            "color: #fff;"
-            "font: 30px;")
-        self.widgets["title"].append(title)
-
-        #button1_frame1
-        button1_frame1 = QPushButton("RUN")
-        button1_frame1.setCursor(QCursor(QtCore.Qt.PointingHandCursor))
-        button1_frame1.setStyleSheet(
-            "*{border: 4px solid '#ccc';" +
-            "border-radius: 15px;" +
-            "font-size: 20px;" +
-            "color: 'white';" +
-            "padding: 5px 5px;" +
-            "margin: 0px 0px;}" + 
-            "*:hover{background: '#369';}")
-        button1_frame1.clicked.connect(self.start)
-
-        self.widgets["button1_frame1"].append(button1_frame1)
-
-        self.grid.addWidget(self.widgets["title"][-1], 0, 0, 1, 4)
-        self.grid.addWidget(self.widgets["button1_frame1"][-1], 1, 0, 1, 4)
-
-    def frame2(self):
-
-        # building widgets - frame 2
-        #-title
-        title_frame2 = QLabel("Testing")
-        title_frame2.setAlignment(QtCore.Qt.AlignCenter)
-        title_frame2.setWordWrap(True)
-        title_frame2.setStyleSheet(
-            "font-size: 35px;" +
-            "color: 'white';"
-        )
-        title_frame2.setFixedHeight(60)
-        self.widgets["title_frame2"].append(title_frame2)
-
-        #-button1
-        button1_frame2 = self.create_buttons("--")
-        button1_frame2.clicked.connect(self.show_frame1)
-        button2_frame2 = self.create_buttons("Connect Motor")
-        button2_frame2.setFixedWidth(140)
-        button2_frame2.clicked.connect(self.createMotorInstance)
-        button3_frame2 = self.create_buttons("Connect Load Cell")
-        button3_frame2.setFixedWidth(160)
-        button3_frame2.clicked.connect(self.createCellInstance)
-        button4_frame2 = self.create_buttons("Start Reading Force")
-        button4_frame2.setFixedWidth(160)
-        button4_frame2.clicked.connect(self.start_worker_readForce)
-        button5_frame2 = self.create_buttons("Start Checking Flags")
-        button5_frame2.setFixedWidth(160)
-        button5_frame2.clicked.connect(self.start_worker_checkFlags)
-        button6_frame2 = self.create_buttons("Start Reading Pos")
-        button6_frame2.setFixedWidth(160)
-        button6_frame2.clicked.connect(self.start_worker_readPosition)
-        button7_frame2 = self.create_buttons("Up")
-        button7_frame2.clicked.connect(self.jogUp)
-        # button7_frame2.clicked.connect(self.waitForTopFlag)
-        button8_frame2 = self.create_buttons("Down")
-        button8_frame2.clicked.connect(self.jogDown)
-        button9_frame2 = self.create_buttons("Home")
-        button9_frame2.clicked.connect(self.home)
-        button10_frame2 = self.create_buttons("Stop")
-        button10_frame2.clicked.connect(self.stop)
-
-        #-labels
-        label1_frame2 = self.create_label("--")
-        label2_frame2 = self.create_label("Motor: Not Connected")
-        label3_frame2 = self.create_label("Load Cell: Not Connected")
-        label4_frame2 = self.create_label("Force: --")
-        label4_1_frame2 = self.create_label("Thread: --")
-        label5_frame2 = self.create_label("Top: -- Home: --")
-        label5_1_frame2 = self.create_label("Thread: --")
-        label6_frame2 = self.create_label("Pos: --")
-        label6_1_frame2 = self.create_label("Thread: --")
-        label7_frame2 = self.create_label("--")
-        label8_frame2 = self.create_label("--")
-
-        # compile widgets - frame 2
-        self.widgets["button1_frame2"].append(button1_frame2)
-        self.widgets["button2_frame2"].append(button2_frame2)
-        self.widgets["button3_frame2"].append(button3_frame2)
-        self.widgets["button4_frame2"].append(button4_frame2)
-        self.widgets["button5_frame2"].append(button5_frame2)
-        self.widgets["button6_frame2"].append(button6_frame2)
-        self.widgets["button7_frame2"].append(button7_frame2)
-        self.widgets["button8_frame2"].append(button8_frame2)
-        self.widgets["button9_frame2"].append(button9_frame2)
-        self.widgets["button10_frame2"].append(button10_frame2)
-        self.widgets["label1_frame2"].append(label1_frame2)
-        self.widgets["label2_frame2"].append(label2_frame2)
-        self.widgets["label3_frame2"].append(label3_frame2)
-        self.widgets["label4_frame2"].append(label4_frame2)
-        self.widgets["label4_1_frame2"].append(label4_1_frame2)
-        self.widgets["label5_frame2"].append(label5_frame2)
-        self.widgets["label5_1_frame2"].append(label5_1_frame2)
-        self.widgets["label6_frame2"].append(label6_frame2)
-        self.widgets["label6_1_frame2"].append(label6_1_frame2)
-        self.widgets["label7_frame2"].append(label7_frame2)
-        self.widgets["label8_frame2"].append(label8_frame2)
-
-        self.grid.addWidget(self.widgets["title_frame2"][-1], 1, 0)
-        self.grid.addWidget(self.widgets["button1_frame2"][-1], 2, 0)
-        self.grid.addWidget(self.widgets["button2_frame2"][-1], 3, 0)
-        self.grid.addWidget(self.widgets["button3_frame2"][-1], 4, 0)
-        self.grid.addWidget(self.widgets["button4_frame2"][-1], 5, 0)
-        self.grid.addWidget(self.widgets["button5_frame2"][-1], 6, 0)
-        self.grid.addWidget(self.widgets["button6_frame2"][-1], 7, 0)
-        self.grid.addWidget(self.widgets["button7_frame2"][-1], 8, 0)
-        self.grid.addWidget(self.widgets["button8_frame2"][-1], 9, 0)
-        self.grid.addWidget(self.widgets["button9_frame2"][-1], 10, 0)
-        self.grid.addWidget(self.widgets["button10_frame2"][-1], 11, 0)
-        self.grid.addWidget(self.widgets["label1_frame2"][-1], 2, 5)
-        self.grid.addWidget(self.widgets["label2_frame2"][-1], 3, 5)
-        self.grid.addWidget(self.widgets["label3_frame2"][-1], 4, 5)
-        self.grid.addWidget(self.widgets["label4_frame2"][-1], 5, 5)
-        self.grid.addWidget(self.widgets["label4_1_frame2"][-1], 5, 2)
-        self.grid.addWidget(self.widgets["label5_frame2"][-1], 6, 5)
-        self.grid.addWidget(self.widgets["label5_1_frame2"][-1], 6, 2)
-        self.grid.addWidget(self.widgets["label6_frame2"][-1], 7, 5)
-        self.grid.addWidget(self.widgets["label6_1_frame2"][-1], 7, 2)
-        self.grid.addWidget(self.widgets["label7_frame2"][-1], 8, 5)
-        self.grid.addWidget(self.widgets["label8_frame2"][-1], 9, 5)
 
     def start(self,widgets):
         self.clear_widgets()
@@ -228,18 +90,21 @@ class Ui_MainWindow(QMainWindow):
 
     def forceReading_return(self, n):
         self.forceReading = n
-        self.widgets["label4_frame2"][-1].setText(f"Force: {self.forceReading} kg")
+        self.m_ui.loadReading_label.setText(f"{self.forceReading} g")
+        # self.widgets["label4_frame2"][-1].setText(f"Force: {self.forceReading} kg")
 
     def topLimit_return(self, b):
         self.topLimit = b
 
     def homeLimit_return(self, b):
         self.homeLimit = b
-        self.widgets["label5_frame2"][-1].setText(f'Top: {self.topLimit} Home: {self.homeLimit}')
+        self.m_ui.flag_reading_label.setText(f'{self.topLimit}/{self.homeLimit}')
+        # self.widgets["label5_frame2"][-1].setText(f'Top: {self.topLimit} Home: {self.homeLimit}')
 
     def positionReading_return(self, n):
         self.positionReading = n
-        self.widgets["label6_frame2"][-1].setText(f'Pos: {self.positionReading}')
+        self.m_ui.positionReading_label.setText(f"{self.positionReading}")
+        # self.widgets["label6_frame2"][-1].setText(f'Pos: {self.positionReading}')
 
     def longer_test_fn(self, progress_callback):
         for n in range(0,5):
@@ -264,17 +129,20 @@ class Ui_MainWindow(QMainWindow):
 
     def createMotorInstance(self):
         if self.motorConnected == False:
-            # self.motor = Motor()
+            self.motor = Motor()
             self.motorConnected = True
-            self.widgets["label2_frame2"][-1].setText("Motor: Connected")
+            self.m_ui.motorStatus_label.setText("Connected")
+            self.start_worker_readPosition()
         else:
             print("motor already connected")
 
     def createCellInstance(self):
         if self.cellConnected == False:
-            # self.cellInstance = LoadCell()
+            self.cellInstance = LoadCell()
             self.cellConnected = True
-            self.widgets["label3_frame2"][-1].setText("Load Cell: Connected")
+            self.m_ui.loadStatus_label.setText("Connected")
+            self.start_worker_readForce()
+            self.start_worker_checkFlags()
         else:
             print("load cell already connected")
 
@@ -302,17 +170,33 @@ class Ui_MainWindow(QMainWindow):
         time.sleep(0.1)
         self.jogging = False
 
+    def startRun(self):
+        try:
+            layerBefore, lbUnit = float(self.m_ui.layerBefore_lineedit.text()), self.m_ui.layerBefore_comboBox.currentIndex()
+            layerAfter = float(self.m_ui.layerAfter_lineedit.text())
+            layerCount = int(self.m_ui.layerCount_lineedit.text())
+            if type(layerBefore) == float and type(layerAfter) == float and type(layerCount) == int:
+                print(layerCount, layerBefore, lbUnit)
+            else:
+                print('ERROR - Run Failed')
+        except:
+            print('ERROR - Invalid Values for Run Command')
+
     # THREAD UTILITY FUNCTIONS
     
     def thread_readForce(self, progress_callback, forceReading_callback, topLimit_callback, homeLimit_callback, positionReading_callback):
-        while True:
-            forceReading = self.cellInstance.readForce()
-            forceReading_callback.emit(forceReading)
-            time.sleep(0.01)
+        if self.force_threadStarted == 0:
+            self.force_threadStarted = 1
+            while True:
+                forceReading = self.cellInstance.readForce()
+                forceReading_callback.emit(forceReading)
+                time.sleep(0.01)
+        else:
+            print("force reading thread already started.")
 
     def start_worker_readForce(self):
         self.readForceStatus = True
-        self.widgets["label4_1_frame2"][-1].setText("Thread: Active")
+        # self.widgets["label4_1_frame2"][-1].setText("Thread: Active")
         self.setWorker(self.thread_readForce)
 
     def threadManager(self, progress_callback, forceReading_callback, topLimit_callback, homeLimit_callback, positionReading_callback):
@@ -333,22 +217,26 @@ class Ui_MainWindow(QMainWindow):
             time.sleep(0.1)
 
     def start_worker_checkFlags(self):
-        self.widgets["label5_1_frame2"][-1].setText("Thread: Active")
-        self.widgets["label5_frame2"][-1].setText(f'Top: {self.topLimit} Home: {self.homeLimit}')
+        # self.widgets["label5_1_frame2"][-1].setText("Thread: Active")
+        # self.widgets["label5_frame2"][-1].setText(f'Top: {self.topLimit} Home: {self.homeLimit}')
         self.setWorker(self.thread_checkFlags)
 
     def thread_readPosition(self, progress_callback, forceReading_callback, topLimit_callback, homeLimit_callback, positionReading_callback):
-        while True:
-            self.motor._checkConnection()
-            if self.jogging == False:
-                position = self.motor.updatePosition()
-                positionReading_callback.emit(position)
-            else:
-                pass
+        if self.position_threadStarted == 0:
+            self.position_threadStarted = 1
+            while True:
+                self.motor._checkConnection()
+                if self.jogging == False:
+                    position = self.motor.updatePosition()
+                    positionReading_callback.emit(position)
+                else:
+                    pass
             time.sleep(0.1)
+        else:
+            print("position reading thread already started.")
 
     def start_worker_readPosition(self):
-        self.widgets["label6_1_frame2"][-1].setText("Thread: Active")
+        # self.widgets["label6_1_frame2"][-1].setText("Thread: Active")
         self.setWorker(self.thread_readPosition)
 
     def waitForTopFlag(self):
